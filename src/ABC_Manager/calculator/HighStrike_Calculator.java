c488d3223dd806cdf8205b3fba88d35332ae55a5
pankajan05 <pankajan05@gmail.com>
2020-09-05 14:42:35 +0530

    create the high strike and boundaries calculation

--- src/ABC_Manager/calculator/HighStrike_Calculator.java
+++ src/ABC_Manager/calculator/HighStrike_Calculator.java
@@ -5,7 +5,8 @@ import ABC_Manager.Team;
 
 public class HighStrike_Calculator implements Calculator{
     private Player player;
-    private float strike = (float) 0.0;
+    private String Venue;
+    private float high_strike = (float) 0.0;
     private Team[] teams;
 
     public HighStrike_Calculator(Team[] teams) {
@@ -14,6 +15,20 @@ public class HighStrike_Calculator implements Calculator{
 
     @Override
     public void calculate() {
-
+        for(int team_counter = 0; team_counter < 2; team_counter++){
+            for(int player_counter = 0; player_counter < 2; player_counter++){
+                float strike = (float) 0.0;
+                for(int score_counter = 0; score_counter < 4; score_counter++){
+                    if(teams[team_counter].players[player_counter].scoreCards[score_counter] != null){
+                        strike = teams[team_counter].players[player_counter].scoreCards[score_counter].getStrike_rate();
+                        if(strike > high_strike){
+                            this.player = teams[team_counter].players[player_counter];
+                            this.high_strike = strike;
+                            this.Venue = teams[team_counter].players[player_counter].scoreCards[score_counter].getVenue();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
