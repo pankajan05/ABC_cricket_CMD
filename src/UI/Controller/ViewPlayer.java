94b9700b260cfaea7658cb6cfbae2af54703911e
pankajan05 <pankajan05@gmail.com>
2020-09-06 11:49:01 +0530

    create a main class for the UI

--- src/UI/Controller/ViewPlayer.java
+++ src/UI/Controller/ViewPlayer.java
@@ -1,15 +1,23 @@
 package UI.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class ViewPlayer {
     @FXML
+    private Button search;
+
+    @FXML
     private TextField teamId;
 
     @FXML
     private TextField playerId;
+
+    @FXML
+    private Label playerInfo;
 
     @FXML
     void Search(MouseEvent event) {
